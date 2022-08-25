256546ad36895ca9a3858cd9a4bd1c3824571b28
Amenable-C <duswo0624@naver.com>
2022-08-25 20:38:38 +0900

    feat : api 명명 규칙 반영

--- src/main/java/com/apptive/parkingpeople/controller/EachParkingLotController.java
+++ src/main/java/com/apptive/parkingpeople/controller/EachParkingLotController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/each")
+@RequestMapping("/parking-lot")
 @RequiredArgsConstructor
 public class EachParkingLotController {
 
