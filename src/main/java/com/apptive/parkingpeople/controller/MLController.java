4c23dd01459a4a194f550dee0b0aa5d4b464c79a
Amenable-C <duswo0624@naver.com>
2022-08-29 14:17:21 +0900

    fix : 어노테이션 수정

--- src/main/java/com/apptive/parkingpeople/controller/MLController.java
+++ src/main/java/com/apptive/parkingpeople/controller/MLController.java
@@ -3,11 +3,13 @@ package com.apptive.parkingpeople.controller;
 import com.apptive.parkingpeople.service.MLService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping
 @RequiredArgsConstructor
 public class MLController {
 
