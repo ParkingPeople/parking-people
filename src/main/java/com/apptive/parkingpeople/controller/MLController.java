93c55a6cb446ab1eb130f3f555665e59aed27a77
Amenable-C <61836238+Amenable-C@users.noreply.github.com>
2022-08-29 22:57:42 +0900

    fix : git action 재시작

--- src/main/java/com/apptive/parkingpeople/controller/MLController.java
+++ src/main/java/com/apptive/parkingpeople/controller/MLController.java
@@ -14,7 +14,7 @@ public class MLController {
 
     private final MLService mlService;
 
-    @PostMapping("/upload/file") 
+    @PostMapping("/upload/file") // test
     public Boolean uploadFile(@RequestParam("id") Long id, @RequestParam("file") MultipartFile file){
 
         double emptyProbability = mlService.setActivityLevel(id, file);
