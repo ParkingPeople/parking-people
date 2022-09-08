b903ed5d1d0dbf8aeb28d08fb3c3943f462b4843
Amenable-C <duswo0624@naver.com>
2022-09-08 13:51:49 +0900

    feat : 4분할 기능과 그에 따른 주차장 개수 문제 해결

--- src/main/java/com/apptive/parkingpeople/controller/RecommendController.java
+++ src/main/java/com/apptive/parkingpeople/controller/RecommendController.java
@@ -10,6 +10,7 @@ import com.apptive.parkingpeople.service.WalkingTimeService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.io.ParseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,18 +39,16 @@ public class RecommendController {
     public ResponseEntity<RecommendResponseDto> recommendParkingLots(@RequestParam("lat") double y, @RequestParam("lon") double x,
                                                                      @RequestParam("range") double range_km) throws ParseException, JsonProcessingException {
 
-//        // 1. 베스트 포인트 확인
-//        Point bestPoint = trafficCongestionService.getBestPointByComparing(y, x, range_km); // lat, lon, range 순순
-//
-//        // 2. 베스트 포인트를 이용하여, 구획 내의 주차장 확인
-//        List<Location> locationsWithinRange = locationService.getLocationsWithinPoint(bestPoint.getCoordinate().y,
-//                bestPoint.getCoordinate().x, range_km);
+        // 1. 베스트 포인트 확인
+        Point bestPoint = trafficCongestionService.getBestPointByComparing(y, x, range_km); // lat, lon, range 순순
 
-        // 원래는 4분할 하고 하나의 영역을 택하는게 맞는데(위의 1번 2번), 개수때문에 일단 아래와 같이 함. // TODO 서버에 띄우고 개수 확인하기
-        // 1` + 2`. 거리안의 주차장 찾기
-        List<Location> locationsWithinRange = locationService.getLocationsWithinPoint(y, x, range_km);
+        // 2. 베스트 포인트를 이용하여, 구획 내의 주차장 확인 // 4분할로 나눈거라서 범위 = 범위 / 2 해야함.
+        List<Location> locationsWithinRange = locationService.getLocationsWithinPoint(bestPoint.getCoordinate().y,
+                bestPoint.getCoordinate().x, range_km / 2);
 
-        System.out.println("범위 안에 있는 주차장의 개수 : " + locationsWithinRange.size());
+        // 1' + 2'. 개수가 3개 미만일 경우 하나의 지역을 선택하는게 아니라 모든 지역 선택하는 것
+        if(locationsWithinRange.size() < 3)
+            locationsWithinRange = locationService.getLocationsWithinPoint(y, x, range_km);
 
         // 3. 구획 내의 주차장 state 업데이트 (24시간 안의 사진들로 state 계산)
         List<ParkingLot> parkingLots = locationsWithinRange
